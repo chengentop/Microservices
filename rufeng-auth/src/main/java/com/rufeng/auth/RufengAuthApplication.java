9d0de7c26de917866d8a78d2a9138d874a9e2bcb
chengen <18291710694@163.com>
2020-12-12 16:29:56 +0800

    完善基础结构

--- rufeng-auth/src/main/java/com/rufeng/auth/RufengAuthApplication.java
+++ rufeng-auth/src/main/java/com/rufeng/auth/RufengAuthApplication.java
@@ -1,9 +1,13 @@
 package com.rufeng.auth;
 
+import com.rufeng.common.security.annotation.EnableRyFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableRyFeignClients
+@SpringCloudApplication
 public class RufengAuthApplication {
 
     public static void main(String[] args) {
